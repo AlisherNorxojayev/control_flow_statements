38e1df6b132a4052dd379508b6c2f3139380cce2
Alisher <norxojayevalisher3@gmail.com>
2022-07-29 13:31:41 +0500

    done

--- bin/if10.dart
+++ bin/if10.dart
@@ -13,4 +13,36 @@
     Returns:
         string: the message to return*/
 
-void main() {}
+void main() {
+  func(2);
+}
+
+int func(int number) {
+  if (number < 0) {
+    print("Freezing");
+  }
+  if (number > 0) {
+    if(number <= 10){
+      print("Very Cold");
+    }
+  }
+  if (number > 10) {
+    if (number <= 20) {
+      print("Cold");
+    }
+  }
+  if (number > 20) {
+    if (number <= 30) {
+      print("normal");
+    }
+  }
+  if (number > 30) {
+    if (number <= 40) {
+      print("Hot");
+    }
+  }
+  if (number > 40) {
+    print("Very Hot");
+  }
+  return 0;
+}
