1974712d6ad86cb42569d8be97bca8f42be67f5b
Alisher <norxojayevalisher3@gmail.com>
2022-07-29 13:29:27 +0500

    done

--- bin/if06.dart
+++ bin/if06.dart
@@ -11,4 +11,42 @@
         c: third number
     Returns:
         string: string with the result*/
-void main() {}
+void main() {
+  print(func(-1, 2, 21));
+}
+
+int func(int a, b, c) {
+  if (a > 0) {
+    if (b > 0) {
+      if (c > 0) {
+        print("there are a lot of positive numbers");
+      }
+      if (c < 0) {
+        print("there are a lot of positive numbers");
+      }
+    }
+    if (c > 0) {
+      print("there are a lot of positive numbers");
+    }
+    if (c < 0) {
+      print("there are a lot of negative numbers");
+    }
+  }
+  if (a < 0) {
+    if (b > 0) {
+      if (c > 0) {
+        print("there are a lot of positive numbers");
+      }
+      if (c < 0) {
+        print("there are a lot of negative numbers");
+      }
+    }
+    if (c > 0) {
+      print("there are a lot of negative numbers");
+    }
+    if (c < 0) {
+      print("there are a lot of negative numbers");
+    }
+  }
+  return 0;
+}
